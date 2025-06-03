ef358b3070d2e0b790b97eb5af435eb27ae0cc8f
GBOKO-git <gboko.amara@uvci.edu.ci>
2025-06-03 19:49:36 +0000

    fix: modification bouton espace membre

--- src/composants/Hero/Hero.jsx
+++ src/composants/Hero/Hero.jsx
@@ -1,6 +1,7 @@
 "use client";
 import { SlArrowLeft } from "react-icons/sl";
 import { FaUsers, FaUserSecret } from "react-icons/fa";
+import { Link } from "react-router-dom";
 
 export const Hero = () => {
   return (
@@ -11,33 +12,37 @@ export const Hero = () => {
             🎓 ASSOCIATION DES ÉLÈVES ET ÉTUDIANTS DE YAOKRO
           </h1>
           <div className="bg-white w-auto h-28 grid items-center justify-start fixed left-0 top-1/2 -translate-y-1/2 rounded-r-3xl pr-2 py-1 z-20">
+            <Link to="/profile" >
             <div className="bg-green-600 h-11 w-full rounded-r-3xl flex items-center justify-between pr-2 gap-3 hover:bg-green-700 transition-colors">
-              <div className="text-white pl-2">
+             <div className="text-white pl-2">
                 <SlArrowLeft />
               </div>
               <div>
-                <a href="/profile" className="hover:text-green-400 font-serif text-white">
+                <h3 className="hover:text-green-400 font-serif text-white">
                   Espace membre
-                </a>
+                </h3>
               </div>
               <div className="bg-green-700 text-white rounded-full size-7 grid items-center justify-center">
                 <FaUserSecret />
               </div>
             </div>
+             </Link>
 
+             <Link to="/espacedonateur" >
             <div className="bg-green-600 h-11 w-full rounded-r-3xl flex items-center justify-between pr-2 gap-3 hover:bg-green-700 transition-colors">
               <div className="text-white pl-2">
                 <SlArrowLeft />
               </div>
               <div>
-                <a href="/espacedonateur" className="hover:text-green-400 font-serif text-white">
+                <h3  className="hover:text-green-400 font-serif text-white">
                   Espace donateur
-                </a>
+                </h3>
               </div>
               <div className="bg-green-700 text-white rounded-full size-7 grid items-center justify-center">
                 <FaUsers />
               </div>
             </div>
+            </Link>
           </div>
 
           <p className="mb-8 text-xl text-white font-semibold drop-shadow">
